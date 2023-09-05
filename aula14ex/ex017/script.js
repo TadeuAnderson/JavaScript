aed21d7fe3d45d59de894b54c49afe5c2943e7eb
Tadeu Anderson <tadeuanderson15@hotmail.com>
2023-09-05 16:01:12 -0300

    m

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,21 @@
-alert('Olá!') 
+function tabuada() {
+    let numero = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if (numero.value.length == 0){
+        window.alert('Por favor, digite um número')
+    }else {
+        let num = Number(numero.value)
+        let c = 1
+        tab.innerHTML = ''
+        while( c <= 10 ) {
+            let item  = document.createElement('option')
+            item.text = `${num} X ${c} = ${num*c}`
+            //faz a seleção do item que foi selecionado
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    } 
+
+}
